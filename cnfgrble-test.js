236657927a718e089ce387a36e745443b8606eb6
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-13 15:54:19 +0800

    Updated server/client test.

--- cnfgrble-test.js
+++ cnfgrble-test.js
@@ -1,7 +1,12 @@
 
 const assert = require( "assert" );
 const cnfgrble = require( "./cnfgrble.js" );
+const harden = require( "harden" );
 
 assert.equal( cnfgrble( "property", { "property": "value" } ), true, "should be true" );
+
+let test = { };
+harden( 123, "sample", test );
+assert.equal( cnfgrble( 123, test ), false, "should be false" );
 
 console.log( "ok" );
